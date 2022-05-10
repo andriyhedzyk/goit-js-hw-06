ab543974a8369ab9341a7be6cce4a854cf182dda
Andriy Hedzyk <andriy.hedzyk@gmail.com>
2022-05-10 00:25:25 -0400

    Update task-02.js
    Update tast 02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+let array = document.getElementById('ingredients')
+let items = ingredients.map(ingredient => {
+    const list = document.createElement('li');
+          list.textContent = ingredient;
+          list.classList.add("item");
+    return list
+});
+array.append(...items);
+console.log(array);
+
