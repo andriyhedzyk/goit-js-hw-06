0d47c495d0ced3bdcdeda78729eaf76ddde5cb53
Andriy Hedzyk <andriy.hedzyk@gmail.com>
2022-05-15 16:47:57 -0400

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const body = document.querySelector("body");
+const btn = document.querySelector(".change-color");
+const nameOfColour = document.querySelector(".color");
+const changeColour = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  nameOfColour.textContent = body.style.backgroundColor;
+};
+btn.addEventListener("click", changeColour);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
