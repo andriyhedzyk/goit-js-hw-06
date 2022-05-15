296c0b7cce5ea5bbbc2f658176c37a5d505e0d25
Andriy Hedzyk <andriy.hedzyk@gmail.com>
2022-05-15 16:03:42 -0400

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  console.log(email.value, password.value);
+    if (email.value === "" || password.value === "") {
+        return alert("Заповніть усі поля");
+    }
+    else {
+        const formData = new FormData(event.currentTarget)
+        console.log(formData)
+        const values = Object.fromEntries(formData.entries())
+        console.log(values);
+    }
+    event.currentTarget.reset();
+});
